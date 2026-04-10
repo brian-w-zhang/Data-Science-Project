1c1c2c6b1abed50597a3b30981fab7e39b4a403e
tuokras <rasmus.tuokko@aalto.fi>
2026-04-10 20:46:56 +0800

    Add repo to colab

--- models/text_branch.py
+++ models/text_branch.py
@@ -1,7 +1,34 @@
+import torch
 from transformers import AutoTokenizer, AutoModel, AutoModelForSequenceClassification
 
 
 MODEL_NAME = "distilbert-base-uncased"
+
+
+class TextDatasetWrapper(torch.utils.data.Dataset):
+    def __init__(self, hf_ds):
+        self.ds = hf_ds
+    def __len__(self):
+        return len(self.ds)
+    def __getitem__(self, idx):
+        item = self.ds[idx]
+        inputs = (item["input_ids"], item["attention_mask"])
+        label  = item["label"]
+        return inputs, label
+
+
+class TextClassificationWrapper(nn.Module):
+    """
+    Wraps a HuggingFace AutoModelForSequenceClassification
+    so that forward(*inputs) returns logits directly.
+    """
+    def __init__(self, hf_model):
+        super().__init__()
+        self.hf_model = hf_model
+
+    def forward(self, input_ids, attention_mask):
+        outputs = self.hf_model(input_ids=input_ids, attention_mask=attention_mask)
+        return outputs.logits
 
 
 def create_text_classifier(num_labels: int = 2):
