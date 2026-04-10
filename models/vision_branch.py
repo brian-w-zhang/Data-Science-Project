8e5c304ff459407c00c6c2763f88a04a31a97930
tuokras <rasmus.tuokko@aalto.fi>
2026-04-10 21:40:45 +0800

    Train text branch

--- models/vision_branch.py
+++ models/vision_branch.py
@@ -1,12 +1,25 @@
 import torch
 import torch.nn as nn
+from torch.utils.data import Dataset
 from torchvision.models import resnet50, ResNet50_Weights
+
+class VisionDatasetWrapper(Dataset):
+    """Wraps a base vision dataset so each item is ((image,), label)."""
+    def __init__(self, base_ds):
+        self.ds = base_ds
+    def __len__(self):
+        return len(self.ds)
+    def __getitem__(self, idx):
+        image, label = self.ds[idx]
+        return (image,), label
+
 
 def build_resnet_classifier(num_classes=2, pretrained=True):
     weights = ResNet50_Weights.DEFAULT if pretrained else None
     model = resnet50(weights=weights)
     model.fc = nn.Linear(model.fc.in_features, num_classes)
     return model
+
 
 def load_vision_encoder(weights_path: str, device):
     """
